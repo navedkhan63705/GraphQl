8c3e70fd4016acc2b2ad9c996f90e9097ccefd1e
navedkhan63705 <BCA22.Naved.Khan@sunstone.edu.in>
2025-09-27 22:29:59 +0530

    second update

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import bodyParser from 'body-parser';
+import { expressMiddleware } from '@apollo/server-express4';
+import cors from 'cors';
+import employeeRouter from './router/Employee.js';
+
 const port = process.env.PORT || 3000;
-const cors = require('cors');
+const app = express();
+ 
+app.use('/employee', employeeRouter);
 
-app.use(cors());
+async function startServer() {
+    const server = new ApolloServer({
+      typeDefs: `
+        type Todo {
+          id: ID!
+          title: String!
+          completed: Boolean!
+        }
+        type Query {
+          getTodos: [Todo]
+        }
+      `,
+      resolvers: {
+        Query: {
+          getTodos: () => [
+            { id: 1, title: "First Todo", completed: false },
+            { id: 2, title: "Second Todo", completed: true },
+          ],
+        },
+      },
+          // disables CSRF check
 
-app.use('/employee',require('./router/Employee'));
+    });
 
-app.listen(port,()=>{
-    console.log('Server is running on port '+ port);
-})
+  await server.start();
+  app.use('/graphql', bodyParser.json(), expressMiddleware(server));
+}
+
+startServer()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.error('Failed to start server', err);
+  });
